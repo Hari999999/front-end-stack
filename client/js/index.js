d9455dd7c425fbc612ba92e6f732b779d555930a
Te Riu Warren <teriu@teriu.name>
2016-12-08 17:19:15 +1300

    Move app.js to index.js

---
+++ client/js/index.js
@@ -0,0 +1,13 @@
+import { foo } from './test';
+import svg4everybody from 'svg4everybody';
+
+console.log('Test', foo());
+
+
+/*
+    Polyfills
+ */
+svg4everybody();
+
+
+console.warn('Fill in the index.js file');
