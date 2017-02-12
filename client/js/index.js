13201681e12760943d045bb419f5f4b86a4ddb28
Hal Smith Stevens <hally9k@gmail.com>
2017-02-13 12:19:23 +1300

    Added redux boilerplate files

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,11 @@
-import { foo } from './test';
+import { configureStore } from './store';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { render } from 'react-dom';
+import Routes from './router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory, Router } from 'react-router';
 import 'sass/index'; // Import CSS
-
 
 /*
     Ensure polyfills only load on older browsers
@@ -17,10 +22,18 @@ if (browserSupportsAllFeatures) {
         initialise();
     });
 }
+
 /*
     Main function
  */
 function initialise() {
-    console.debug('Test', foo());
-    console.warn('Fill in the index.js file'); // eslint-disable-line
+    const reduxStore = configureStore();
+    const history = syncHistoryWithStore(browserHistory, reduxStore);
+
+    render((
+        <Provider store={reduxStore}>
+            <Router history={history} routes={Routes}>
+            </Router>
+        </Provider>
+    ), document.getElementById('root'));
 }
