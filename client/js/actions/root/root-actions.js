460beef2a9ab2ba9411fdcfe912796b941d9e182
Hal Smith Stevens <hal@pixelfusion.co.nz>
2017-03-19 20:40:12 +1300

    fixed root action creator style

--- client/js/actions/root/root-actions.js
+++ client/js/actions/root/root-actions.js
@@ -1,11 +1,8 @@
 export const REPLY_TO_GREETING = 'ROOT__REPLY_TO_GREETING';
 
-
-export function replyToGreeting(reply) {
-    return {
-        type: REPLY_TO_GREETING,
-        payload: {
-            reply
-        }
-    };
-}
+export const replyToGreeting = (reply) => ({
+    type: REPLY_TO_GREETING,
+    payload: {
+        reply
+    }
+});
