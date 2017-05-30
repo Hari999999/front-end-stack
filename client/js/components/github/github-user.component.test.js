56d6aff2759681c7374781da20d9de311589e338
hally9k <hally9k@gmail.com>
2017-05-31 11:46:55 +1200

    added snapshot tests

--- client/js/components/github/github-user.component.test.js
+++ client/js/components/github/github-user.component.test.js
@@ -1,31 +1,28 @@
 import GithubUser from 'components/github/github-user.component';
-import { mount } from 'enzyme';
 import React from 'react';
+import { mount, shallow } from 'enzyme';
 
-function setup() {
-    const props = {
-        user: {
-            avatar: 'https://myimage.com', // eslint-disable-line camelcase
-            username: 'hally9k',
-            id: '123456'
-        }
-    };
-
-    const enzymeWrapper = mount(<GithubUser {...props} />);
-
-    return {
-        props,
-        enzymeWrapper
-    };
-}
+const props = {
+    user: {
+        avatar: 'https://myimage.com',
+        username: 'hally9k',
+        id: '123456'
+    }
+};
 
 describe('components', () => {
     describe('GithubSearch', () => {
         it('should render the user when given a valid user object', () => {
-            const { enzymeWrapper } = setup('user');
+            const enzymeWrapper = mount(<GithubUser {...props} />);
+
             expect(enzymeWrapper.find('div.github-user img').prop('src')).toBe('https://myimage.com');
             expect(enzymeWrapper.find('div.github-user h2').text()).toBe('hally9k');
             expect(enzymeWrapper.find('div.github-user h4').text()).toBe('ID: 123456');
+        });
+
+        it('mounts the component', () => {
+            const enzymeWrapper = shallow(<GithubUser {...props} />);
+            expect(enzymeWrapper).toMatchSnapshot();
         });
     });
 });
