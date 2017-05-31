91fa0f3b2f7756a857fe9efa5a72cb2f5f90b9eb
hally9k <hally9k@gmail.com>
2017-05-31 20:53:49 +1200

    fixed github epic to use fetch

--- client/js/epics/root/root.epics.js
+++ client/js/epics/root/root.epics.js
@@ -1,21 +1,22 @@
-import * as rootActions from 'actions/root/root.actions';
-import { ajax } from 'rxjs/observable/dom/ajax';
 import { combineEpics } from 'redux-observable';
-import { Observable } from 'rxjs/Observable';
 import pingEpics from 'epics/root/ping/ping.epics';
 import starwarsEpics from 'epics/root/starwars/starwars.epics';
+import {
+    GET_GITHUB_USER,
+    getGithubUserFailure,
+    getGithubUserSuccess
+} from 'actions/root/root.actions';
 
-export const githubEpic = (action$) =>
-      action$.ofType(rootActions.GET_GITHUB_USER)
-      .mergeMap((action) =>
-         ajax
-            .getJSON(`https://api.github.com/users/${action.payload}`)
-            .map((response) => {
-                return rootActions.getgithubUserSuccess(response);
-            })
-            .catch((error) => Observable.of(
-                rootActions.getgithubUserFailure(error)
-            ))
-       );
+const githubEpic = (action$) =>
+    action$.ofType(GET_GITHUB_USER).mergeMap((action) =>
+        fetch(`https://api.github.com/users/${action.payload}`)
+            .then((response) => response.json())
+            .then((response) => getGithubUserSuccess({
+                avatar: response.avatar_url,
+                username: response.login,
+                id: response.id
+            }))
+            .catch((error) => getGithubUserFailure(error))
+    );
 
 export default combineEpics(pingEpics, starwarsEpics, githubEpic);
