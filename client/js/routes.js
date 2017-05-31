06d1d479ab40df7b558ae1af1e923679ab9f16aa
hally9k <hally9k@gmail.com>
2017-05-31 19:03:19 +1200

    upgraded front-end-scripts

--- client/js/routes.js
+++ client/js/routes.js
@@ -2,11 +2,18 @@ import Github from 'components/github/github.container';
 import Ping from 'components/ping/ping.container';
 import React from 'react';
 import StarWars from 'components/starwars/starwars.container';
-import { Route, Switch } from 'react-router';
+import { Link, Route, Switch } from 'react-router';
+
+const nav = <div>
+    <Link to="/github">github</Link> •
+    <Link to="/ping">ping</Link> •
+    <Link to="/starwars">starwars</Link>
+</div>;
 
 export default
 <Switch>
-    <Route exact path="/" component={Github}></Route>
-    <Route path="/ping" component={Ping}></Route>
-    <Route path="/starwars" component={StarWars}></Route>
+    <Route path="/" component={nav} />
+    <Route path="/" component={Github} />
+    <Route path="/ping" exact component={Ping} />
+    <Route path="/starwars" exact component={StarWars} />
 </Switch>;
